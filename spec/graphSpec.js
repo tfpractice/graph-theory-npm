3586e4db75c3cf12ffe0c4a5d4444b3b82c642d0
theory friction practice <jackson.brebnor@gmail.com>
2016-06-10 16:13:48 -0400

    removes console statements from Graph module

--- spec/graphSpec.js
+++ spec/graphSpec.js
@@ -82,7 +82,7 @@ describe('Graph', function() {
         describe('hasPath', function() {
             it('determines if a path exists between to vertices in a graph', function() {
                 var result = myGraph.hasPath(v1, v3);
-                console.log(result);
+                // console.log(result);
                 expect(myGraph.hasPath(v1, v3)).toBeTrue();
             });
         });
