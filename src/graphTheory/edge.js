18f65cc24d0cc0d605e4c9bac4c01ee0925cb1c2
theory friction practice <jackson.brebnor@gmail.com>
2016-06-10 21:15:08 -0400

    completely nullifies v1 edge class, specs pass

--- src/graphTheory/edge.js
+++ src/graphTheory/edge.js
@@ -1,4 +1,4 @@
-var Vertex = require('./vertex');
+// var Vertex = require('./vertex');
 /**
  * represents an Edge
  * @exports Edge
@@ -6,28 +6,28 @@ var Vertex = require('./vertex');
  * @memberOf! module:graphTheory
  * @param {Vertex}     sVertex source vertex
  * @param {Vertex}     dVertex destination vertex
- * @param {Number} w weight
+ * @param {Number} w ytrweight
  * @property {Vertex} source the source vertex
  * @property {Vertex} dest the destination vertex
  * @property {Number} w the weight of the edge
  */
-module.exports = function Edge(sVertex = new Vertex(), dVertex = new Vertex(), w = 0) {
-    /**
-     * the source vertex
-     * @type {Vertex}
-     */
-    this.source = sVertex;
-    /**
-     * the destination vertex
-     * @type {Vertex}
-     */
-    this.dest = dVertex;
-    /**
-     * the weight of the edge
-     * @type {Number}
-     */
-    this.weight = w;
-};
+// module.exports = function Edge(sVertex = new Vertex(), dVertex = new Vertex(), w = 0) {
+/**
+ * the source vertex
+ * @type {Vertex}
+ */
+// this.source = sVertex;
+/**
+ * the destination vertex
+ * @type {Vertex}
+ */
+// this.dest = dVertex;
+/**
+ * the weight of the edge
+ * @type {Number}
+ */
+// this.weight = w;
+// };
 
 /**
  * [An Edge]{@link module:graphTheory.Edge}
