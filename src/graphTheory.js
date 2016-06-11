31189a4acdfeb08bd9173554ea9bb39fe0c9bb35
theory friction practice <jackson.brebnor@gmail.com>
2016-06-10 23:34:21 -0400

    adds EdgeArray to GraphTHeory exports. creates EdgeArraySpec.js

--- src/graphTheory.js
+++ src/graphTheory.js
@@ -5,5 +5,6 @@ exports.Vertex = require('./graphTheory/vertex');
 exports.Node = require('./graphTheory/node');
 exports.NodeArray = require('./graphTheory/nodeArray');
 exports.DirectedEdge = require('./graphTheory/directedEdge');
-exports.Edge = require('./graphTheory/edge');
+exports.Edge = require('./graphTheory/edg');
+exports.EdgeArray = require('./graphTheory/edgeArray');
 exports.Graph = require('./graphTheory/graph');
