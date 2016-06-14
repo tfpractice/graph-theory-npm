0b834dc3fd02cfc0e7f055029d3ad2948ba1bf99
theory friction practice <jackson.brebnor@gmail.com>
2016-06-14 11:40:38 -0400

    eComp.containsEdge

--- src/graphTheory/edgeComponent.js
+++ src/graphTheory/edgeComponent.js
@@ -8,6 +8,19 @@ class EdgeComponent {
         this.edges = new EdgeArray(e1);
 
     }
+    containsEdge(edgeArg) {
+        return this.edges.contains(edgeArg);
+    }
+    // containsEdge(edgeArg) {
+    // return this.edges.contains(edgeArg);
+    // }
+    // addEdge(edgeArg) {
+    // this.edges.push(edgeArg);
+    // this.arity++;
+    // }
+    // connected(n1, n2) {
+    // return (this.containsEdge(n1) && this.containsEdge(n2));
+    // }
 
     // methods
 }
