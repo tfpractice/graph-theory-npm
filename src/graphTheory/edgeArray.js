bd1e353a70ed1ce4650aa4841a8f6e54e67b70c3
theory friction practice <jackson.brebnor@gmail.com>
2016-06-14 10:55:55 -0400

    EdgeArray.intersection/intersects

--- src/graphTheory/edgeArray.js
+++ src/graphTheory/edgeArray.js
@@ -23,7 +23,39 @@ class EdgeArray extends Array {
             return nArray;
         }, new NodeArray());
     }
-    //
-    // methods
+    intersection(altArray) {
+        return this.filter(currEdge => altArray.contains(currEdge) === true);
+    }
+    intersects(altArray) {
+        return this.some(currEdge => altArray.contains(currEdge) === true);
+    }
+    // difference(altArray) {
+    // let diffArray = new NodeArray();
+
+    // this.reduce((dArray, currEdge) => {
+    // if (!altArray.contains(currEdge)) dArray.push(currEdge);
+    // return dArray;
+    // }, diffArray);
+    // altArray.reduce((dArray, altNode) => {
+    // if (!this.contains(altNode)) dArray.push(altNode);
+    // return dArray;
+    // }, diffArray);
+
+    // return diffArray;
+
+    // }
+    // hasDistinctNodes(altArray) {
+    // return altArray.some(altNode => !this.contains(altNode));
+    // }
+    // union(altArray) {
+    // let uArray = new NodeArray();
+    // this.filter(currEdge => uArray.push(currEdge));
+    // altArray.filter(altNode => uArray.push(altNode));
+    // return uArray;
+    // }
+    // unionize(altArray) {
+    // this.difference(altArray).forEach(dNode => this.push(dNode));
+    // }
+
 }
 module.exports = EdgeArray;
