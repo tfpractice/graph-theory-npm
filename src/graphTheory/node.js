d9fefc51bcdc9f1eaa3aeeb8811655e5de82ae92
theory friction practice <jackson.brebnor@gmail.com>
2016-06-20 11:37:36 -0400

    documents Ndoe class

--- src/graphTheory/node.js
+++ src/graphTheory/node.js
@@ -1,33 +1,37 @@
 /**
- * represent a Vertex
- * @exports Vertex
+ * represent a Node
+ * @exports Node
  * @constructor
  * @memberOf module:graphTheory
- * @param {String} [label = 'default']  [label property]{@link module:graphTheory.Vertex#label}
+ * @param {String} [label = 'default']  [label property]{@link module:graphTheory.Node#label}
+ * @param {Object} data the nodes data
  */
-// module.exports = function Node(label) {
-/**
- * the vertex identifier
- * @type {String}
- */
-// this.label = label;
-// };
-
-/**
- * [A Vertex]{@link module:graphTheory.Vertex}
- * @typedef {module:graphTheory.Vertex} Vertex
- */
-
-
 class Node {
     constructor(label, data) {
+        /**
+         * the node identifier
+         * @type {String}
+         */
         this.label = label;
+        /**
+         * the node data
+         * @type {Object}
+         */
         this.data = data;
 
     }
+    /**
+     * Checks for identity via label attribute
+     * @param  {Node}  newNode the node to be compared
+     * @return {Boolean}
+     */
     isIdentical(newNode) {
         return (this.label) ? this.label == newNode.label : false;
     }
 }
 
 module.exports = Node;
+/**
+ * [A Node]{@link module:graphTheory.Node}
+ * @typedef {module:graphTheory.Node} Node
+ */
