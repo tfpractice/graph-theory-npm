1de6ff03d4621c8caf9d9c5baa8e146acda5212e
theory friction practice <jackson.brebnor@gmail.com>
2016-07-02 10:37:35 -0400

    checks specs after file removals

--- src/graphTheory.js
+++ src/graphTheory.js
@@ -1,12 +1,9 @@
 /**
  * @module graphTheory
  */
-// exports.Vertex = require('./graphTheory/vertex');
 exports.Node = require('./graphTheory/node');
 exports.NodeArray = require('./graphTheory/nodeArray');
 exports.DirectedEdge = require('./graphTheory/directedEdge');
 exports.Edge = require('./graphTheory/edge');
 exports.EdgeArray = require('./graphTheory/edgeArray');
 exports.Graph = require('./graphTheory/graph');
-// exports.Component = require('./graphTheory/component');
-// exports.EdgeComponent = require('./graphTheory/edgeComponent');
