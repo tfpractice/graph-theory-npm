cd7f5ef95e363178cd3da95557ada1678e2dda43
theory friction practice <jackson.brebnor@gmail.com>
2016-07-09 18:11:44 -0400

    RArray#push

--- spec/RobustArraySpec.js
+++ spec/RobustArraySpec.js
@@ -26,7 +26,7 @@ describe('RobustArray', () => {
         });
         describe('when BaseType is Node', () => {
             describe('isValid(argObj)', () => {
-                it('returns true if argNode is an instanceof Node', function() {
+                it('returns true if argObj is an instanceof Node', function() {
                     expect(myArray.isValid(myNode)).toBeTrue();
                 });
                 it('returns false if argObj is not an instance of Node', function() {
@@ -35,5 +35,93 @@ describe('RobustArray', () => {
             });
         });
     });
+
+    fdescribe('push(argObj) ', () => {
+        describe('when argObj is an instanceof Node', () => {
+            it('calls super [Array.push] and returns modified array', function() {
+                var currLength = myArray.length;
+                let newNode = new Node('newNode');
+                var newLength = currLength + 1;
+                expect(myArray.push(newNode)).toEqual(myArray);
+            });
+        });
+        describe('when argObj is not an instanceof Node', () => {
+            it('returns the unmodified array', function() {
+                expect(myArray.push(2)).toBeArray();
+            });
+            it('does not increment length', function() {
+                var currLength = myArray.length;
+                myArray.push(2);
+                expect(myArray.length).toEqual(currLength);
+            });
+        });
+    });
+    describe('intersection(altArray)', function() {
+        it('retuns an array of nodes shared by two nodeArrays', function() {
+            expect(myArray.intersection(myAltArray)).toBeArray();
+        });
+    });
+    describe('intersects', function() {
+        it('determines if two arrays share any nodes', function() {
+            expect(myArray.intersects(myAltArray)).toBeTrue();
+        });
+    });
+    describe('difference', function() {
+        it('returns an array of nodes contained in the caller but not in the argument', function() {
+            expect(myArray.difference(myAltArray)).toBeArray();
+        });
+    });
+    describe('hasDistinctNodes', function() {
+        it('determines if caller has nodes that argument does not', function() {
+            expect(myArray.hasDistinctNodes(myAltArray)).toBeTrue();
+        });
+    });
+    describe('#isEquivalent', function() {
+        it('returns true if arrays are of same size and are subsets of each other', function() {
+            let subArray = new NodeArray(myNode);
+            let subArray2 = new NodeArray(myNode);
+            expect(subArray2.isEquivalent(subArray)).toBeTrue();
+        });
+    });
+    describe('#isSubset', () => {
+        it('returns true if every node in the callers is in the argument', function() {
+            let subArray = new NodeArray(myNode);
+            expect(subArray.isSubset(myArray)).toBeTrue();
+        });
+    });
+    describe('#hasSameSize', () => {
+        it('returns if the arrays share length', function() {
+            expect(myArray.hasSameSize(myAltArray)).toBeTrue();
+        });
+    });
+    describe('union', function() {
+        it('returns an array of all nodes between two array', function() {
+            expect(myArray.union(myAltArray)).toBeArray();
+        });
+    });
+    describe('unionize', function() {
+        it('combines the nodes of both arrays', function() {
+            myArray.unionize(myAltArray);
+            var nodeUnion = myArray.union(myAltArray);
+
+            expect(myArray).toEqual(nodeUnion);
+        });
+        it('retuns the modified array', function() {
+            let uArr = myArray.unionize(myAltArray);
+            var nodeUnion = myArray.union(myAltArray);
+            expect(uArr).toBeArray();
+
+        });
+    });
+    describe('nodeComplement()', () => {
+        it('returns an array of all nodes excluding the argument', function() {
+            expect(myArray.nodeComplement(myNode)).toBeArray();
+        });
+    });
+
+
+
+
+
 
 });
--- src/graphTheory/robustArray.js
+++ src/graphTheory/robustArray.js
@@ -16,7 +16,121 @@ var RobustArray = BaseType => class extends Array {
     contains(argObj) {
         return this.some(el => el.isEquivalent(argObj));
     }
-    // methods
+    push(argObj) {
+        if ((this.isValid(argObj) && !(this.contains(argObj)))) {
+            super.push(argObj);
+        }
+        return this;
+    }
+    /**
+     * returns an array shared nodes between two sets
+     * @param  {NodeArray} altArray the array to check
+     * @return {NodeArray} the shared nodes
+     */
+    // intersection(altArray) {
+    //     return this.filter(currNode => altArray.contains(currNode) === true);
+    // }
+    // /**
+    //  * checks for presence of shared nodes between two sets
+    //  * @param  {NodeArray} altArray the array to check
+    //  * @return {Boolean}
+    //  */
+    // intersects(altArray) {
+    //     return this.some(currNode => altArray.contains(currNode) === true);
+    // }
+    // /**
+    //  * returns an array nodes in this array not included in the alternate array
+    //  * @param  {NodeArray} altArray the array to check
+    //  * @return {NodeArray} the unshared nodes
+    //  */
+    // difference(altArray) {
+    //     return this.filter(n => !altArray.contains(n));
+    // }
+    // /**
+    //  * checks for presence of unshared nodes between two sets
+    //  * @param  {NodeArray} altArray the array to check
+    //  * @return {Boolean}
+    //  */
+    // hasDistinctNodes(altArray) {
+    //     return this.some(myNode => !altArray.contains(myNode));
+    //     // return altArray.some(altNode => !this.contains(altNode));
+    // }
+    // hasSameSize(altArray) {
+    //     return this.length === altArray.length;
+    // }
+    // isSubset(altArray) {
+    //     return this.every(myNode => altArray.contains(myNode));
+    // }
+    // isEquivalent(altArray) {
+    //     return (this.hasSameSize(altArray) && this.isSubset(altArray));
+    // }
+    // /**
+    //  * returns a combined array of nodes belonging to this and the alternate arrays
+    //  * @param  {NodeArray} altArray the array to combine
+    //  * @return {NodeArray} the nodes
+    //  */
+    // union(altArray) {
+    //     let uArray = new NodeArray();
+    //     this.forEach(currNode => uArray.push(currNode));
+    //     altArray.forEach(altNode => uArray.push(altNode));
+    //     return uArray;
+    // }
+    // /**
+    //  * combines all the nodes into this nodeArray
+    //  * @param  {NodeArray} altArray the array to check
+    //  */
+    // unionize(altArray) {
+    //     altArray.difference(this).forEach(dNode => this.push(dNode));
+    //     return this;
+    // }
+    // /**
+    //  * forces return type to a NodeArray
+    //  * @param  {...[type]} args the arguments
+    //  * @return {[NodeArray]}
+    //  */
+    // filter(...args) {
+    //     return NodeArray.from(super.filter(...args));
+    // }
+    // /**
+    //  * forces return type to a NodeArray
+    //  * @param  {...[type]} args the arguments
+    //  * @return {[NodeArray]}
+    //  */
+    // slice(...args) {
+    //     return NodeArray.from(super.slice(...args));
+    // }
+    // /**
+    //  * forces return type to a NodeArray
+    //  * @param  {...[type]} args the arguments
+    //  * @return {[NodeArray]}
+    //  */
+    // concat(...args) {
+    //     return NodeArray.from(super.concat(...args));
+    // }
+    // /**
+    //  * forces return type to a NodeArray
+    //  * @param  {...[type]} args the arguments
+    //  * @return {[NodeArray]}
+    //  */
+    // splice(...args) {
+    //     return NodeArray.from(super.splice(...args));
+    // }
+    // removeNode(nArg) {
+    //     let nPos = this.indexOf(nArg);
+    //     return (nPos > -1) ? this.splice(nPos, 1) : false;
+    // }
+
+
+    // nodeComplement(nArg) {
+    //     return this.filter(n => n.isEquivalent(nArg));
+    // }
+    // clear() {
+    //     this.splice(0);
+    //     return this;
+    // }
+    // copy() {
+    //     return this.slice(0);
+    // }
 };
 
 module.exports.SetifyType = RobustArray;
