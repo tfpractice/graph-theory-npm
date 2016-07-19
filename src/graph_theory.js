e744464f5cca167bad1cdca657d7978b88b9878c
theory friction practice <jackson.brebnor@gmail.com>
2016-07-19 15:15:35 -0400

    snake_cases module folder

--- src/graph_theory.js
+++ src/graph_theory.js
@@ -1,14 +1,11 @@
 /**
  * @module GraphTheory
  */
-
-
-
-exports.Node = require('./graphTheory/node');
-exports.RobustArray = require('./graphTheory/robust_array');
-exports.NodeArray = require('./graphTheory/node_array');
-exports.DirectedEdge = require('./graphTheory/directed_edge');
-exports.Edge = require('./graphTheory/edge');
-exports.EdgeArray = require('./graphTheory/edge_array');
-exports.ComponentArray = require('./graphTheory/component_array');
-exports.Graph = require('./graphTheory/graph');
+exports.Node = require('./graph_theory/node');
+exports.RobustArray = require('./graph_theory/robust_array');
+exports.NodeArray = require('./graph_theory/node_array');
+exports.DirectedEdge = require('./graph_theory/directed_edge');
+exports.Edge = require('./graph_theory/edge');
+exports.EdgeArray = require('./graph_theory/edge_array');
+exports.ComponentArray = require('./graph_theory/component_array');
+exports.Graph = require('./graph_theory/graph');
